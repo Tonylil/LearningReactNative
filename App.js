691c3bb8d482eed3756a047a2432bf4c07f63d34
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-08-06 15:57:33 -0400

    Running into errors

--- App.js
+++ App.js
@@ -1,22 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import React from 'react';
+//import { StyleSheet, Text, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
-import MapScreen from './screens/MapScreen'
+//import { MapView } from "expo";
+//import MapScreen from './screens/MapScreen'
 
-const Stack = createStackNavigator()
-
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Map" component={MapScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+//const Stack = createStackNavigator()
+import React from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+export default
+class App extends React {
+  render() {
+    return (
+      <MapView
+         style={{ flex: 1 }}
+         provider={PROVIDER_GOOGLE}
+         showsUserLocation
+         initialRegion={{
+         latitude: 37.78825,
+         longitude: -122.4324,
+         latitudeDelta: 0.0922,
+         longitudeDelta: 0.0421}}
+      />
+    );
+  }
 }
 
 const styles = StyleSheet.create({
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -4,13 +4,22 @@
 
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { MapView } from "expo";
 
 const MapScreen = props => {
     return (
-        <View>
-            <Text>The MapScreen</Text>
-        </View>
-    )
+      <MapView
+        style={{
+          flex: 1
+        }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+      />
+    );
 }
 
 const styles = StyleSheet.create({
